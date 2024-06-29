16390cbe301762fc473ad7c7f2a10b10841ebb22
Cristiano Sobral <cssobral2013@gmail.com>
2024-06-29 20:06:35 -0300

    Upload version 2.7

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Heraclito
 Is developed from Commissioner
+
+Main changes include:
+ - Remastered to use only wght axis;
+ - Modified G and J;
+ - Added tabular figures;
+ - Made figures reach caps height;
+ - Added contextual ligatures;
+ - Imported question mark from Source Serif 4, then adapted;
+ - And others.
---
+++ generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/Heraclito-Thin.ttf'
+woffPath = './web/Heraclito-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Heraclito-ThinItalic.ttf'
+woffPath = './web/Heraclito-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Heraclito-ExtraLight.ttf'
+woffPath = './web/Heraclito-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Heraclito-ExtraLightItalic.ttf'
+woffPath = './web/Heraclito-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/Heraclito-Light.ttf'
+woffPath = './web/Heraclito-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-LightItalic.ttf'
+woffPath = './web/Heraclito-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-LightItalic.ttf'
+woffPath = './web/Heraclito-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-Regular.ttf'
+woffPath = './web/Heraclito-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-Italic.ttf'
+woffPath = './web/Heraclito-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-Medium.ttf'
+woffPath = './web/Heraclito-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-MediumItalic.ttf'
+woffPath = './web/Heraclito-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBold.ttf'
+woffPath = './web/Heraclito-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBoldItalic.ttf'
+woffPath = './web/Heraclito-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBold.ttf'
+woffPath = './web/Heraclito-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBoldItalic.ttf'
+woffPath = './web/Heraclito-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-Bold.ttf'
+woffPath = './web/Heraclito-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-BoldItalic.ttf'
+woffPath = './web/Heraclito-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-ExtraBold.ttf'
+woffPath = './web/Heraclito-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-ExtraBoldItalic.ttf'
+woffPath = './web/Heraclito-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-Black.ttf'
+woffPath = './web/Heraclito-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Heraclito-BlackItalic.ttf'
+woffPath = './web/Heraclito-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/Heraclito-Thin.ttf'
+woff2Path = './web/Heraclito-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Heraclito-ThinItalic.ttf'
+woff2Path = './web/Heraclito-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Heraclito-ExtraLight.ttf'
+woff2Path = './web/Heraclito-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Heraclito-ExtraLightItalic.ttf'
+woff2Path = './web/Heraclito-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/Heraclito-Light.ttf'
+woff2Path = './web/Heraclito-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-LightItalic.ttf'
+woff2Path = './web/Heraclito-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-LightItalic.ttf'
+woff2Path = './web/Heraclito-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-Regular.ttf'
+woff2Path = './web/Heraclito-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-Italic.ttf'
+woff2Path = './web/Heraclito-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-Medium.ttf'
+woff2Path = './web/Heraclito-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-MediumItalic.ttf'
+woff2Path = './web/Heraclito-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBold.ttf'
+woff2Path = './web/Heraclito-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBoldItalic.ttf'
+woff2Path = './web/Heraclito-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBold.ttf'
+woff2Path = './web/Heraclito-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-SemiBoldItalic.ttf'
+woff2Path = './web/Heraclito-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-Bold.ttf'
+woff2Path = './web/Heraclito-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-BoldItalic.ttf'
+woff2Path = './web/Heraclito-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-ExtraBold.ttf'
+woff2Path = './web/Heraclito-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-ExtraBoldItalic.ttf'
+woff2Path = './web/Heraclito-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-Black.ttf'
+woff2Path = './web/Heraclito-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Heraclito-BlackItalic.ttf'
+woff2Path = './web/Heraclito-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
